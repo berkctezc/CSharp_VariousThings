6123aed32956d90c3b2a23522fa1ae08a1563888
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-08-15 15:01:19 +0300

    added general exception handling

---
+++ ExceptionsDemo_ConsoleUI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ExceptionsDemo_Library;
+
+namespace ExceptionsDemo_ConsoleUI
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var demo = new DemoCode();
+
+            // higher the better to put try catch (UI)
+            try
+            {
+                var result = demo.GrandparentMethod(4);
+                Console.WriteLine($"The value at the given pos is {result}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Bad argument given!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+
+                // access to inner exception and its data
+                var inner = ex.InnerException;
+
+                while (inner != null)
+                {
+                    Console.WriteLine(inner.StackTrace);
+                    inner = inner.InnerException;
+                }
+            }
+        }
+    }
+}
---
+++ ExceptionsDemo_Library/DemoCode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ExceptionsDemo_Library
+{
+    public class DemoCode
+    {
+        public int GrandparentMethod(int position)
+        {
+            int output = default;
+
+            Console.WriteLine("Open DB Conn");
+
+            try
+            {
+                output = ParentMethod(position);
+
+                //if (position < 0)
+                //{
+                //    throw new IndexOutOfRangeException("The value must be >=0");
+                //}
+            }
+            catch (Exception ex)
+            {
+                // Logging Here
+                throw new ArgumentException("You passed in bad data", ex);
+                // user friendly exception with inner exception and it could be caught later from upper methods
+            }
+            finally // runs no matter what
+            {
+                Console.WriteLine("Close DB Conn");
+            }
+
+            return output;
+        }
+
+
+        public int ParentMethod(int position)
+        {
+            return GetNumber(position);
+        }
+
+        public int GetNumber(int position)
+        {
+            int output = default;
+            var numbers = new[] { 1, 4, 7, 2 };
+            output = numbers[position];
+            return output;
+        }
+
+    }
+}
