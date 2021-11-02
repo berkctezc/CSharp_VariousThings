f513e6b742f8bebc52dae64857982b6cd24aba3c
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-11-02 13:48:52 +0300

    updated worker service and readme

---
+++ WorkerService_Microsoft/Program.cs
@@ -0,0 +1,46 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace WorkerService_Microsoft
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .WriteTo.File(@"C:\temp\workerservice\LogFile.txt")
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("starting up");
+                CreateHostBuilder(args).Build().Run();
+                return;
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Problem");
+                throw;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .UseSerilog();
+    }
+}
---
+++ WorkerService_Microsoft/Worker.cs
@@ -0,0 +1,53 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WorkerService_Microsoft
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private HttpClient client;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            client = new HttpClient();
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            client.Dispose();
+            _logger.LogInformation("Service has been stopped...");
+            return base.StopAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var result = await client.GetAsync("https://www.google.com/", stoppingToken);
+
+                if (result.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation("Website is up. {StatusCode} at {time}", result.StatusCode, DateTimeOffset.Now);
+                }
+                else
+                {
+                    _logger.LogError("Website is down. {StatusCode}", result.StatusCode);
+                }
+
+                //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(2000, stoppingToken);
+            }
+        }
+    }
+}
