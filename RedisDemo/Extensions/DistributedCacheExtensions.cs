56359adec689a414eba543f622ee45612b9c7029
berkctezc <berkcantezcaner@gmail.com>
2021-11-21 15:09:32 +0300

    redisdemo: added get and set helper extension methods

---
+++ RedisDemo/Extensions/DistributedCacheExtensions.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace RedisDemo.Extensions
+{
+    public static class DistributedCacheExtensions
+    {
+        public static async Task SetRecordAsync<T>(this IDistributedCache cache,
+        string recordId,
+        T data,
+        TimeSpan? absoluteExpireTime = null,
+        TimeSpan? unusedExpireTime = null)
+        {
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60),
+                SlidingExpiration = unusedExpireTime
+            };
+            var jsonData = JsonSerializer.Serialize(data);
+            await cache.SetStringAsync(recordId, jsonData, options);
+        }
+
+        public static async Task<T> GetRecordAsync<T>(this IDistributedCache cache, string recordId)
+        {
+            var jsonData = await cache.GetStringAsync(recordId);
+
+            if (jsonData is null)
+            {
+                return default;
+            }
+
+            return JsonSerializer.Deserialize<T>(jsonData);
+        }
+    }
+
+}
