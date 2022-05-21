2f4d03bf2ba707551245394bd7158b712cfdbc24
berkctezc <berkcantezcaner@gmail.com>
2022-05-21 18:44:07 +0300

    added bogus demo

---
+++ FakeDataWithBogus/BillingDetails.cs
@@ -0,0 +1,18 @@
+﻿namespace FakeDataWithBogus;
+
+public class BillingDetails
+{
+    public string CustomerName { get; set; }
+
+    public string Email { get; set; }
+
+    public string Phone { get; set; }
+
+    public string AddressLine { get; set; }
+
+    public string City { get; set; }
+
+    public string PostCode { get; set; }
+
+    public string Country { get; set; }
+}
---
+++ FakeDataWithBogus/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace FakeDataWithBogus;
+
+public class Order
+{
+    public Guid Id { get; set; }
+    public string ItemName { get; set; }
+    public decimal Price { get; set; }
+    public string Currency { get; set; }
+    public BillingDetails BillingDetails { get; set; }
+}
