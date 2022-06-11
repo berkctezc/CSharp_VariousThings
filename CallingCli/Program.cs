41316c0aa85cd63e82ffb8064d9296f5e3402b69
berkctezc <berkcantezcaner@gmail.com>
2022-06-11 11:12:47 +0300

    cli wrap demo added

---
+++ CallingCli/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CliWrap;
+using CliWrap.Buffered;
+
+var dockerResults = await Cli.Wrap("docker")
+    .WithArguments(new[] {"--version"})
+    .ExecuteBufferedAsync();
+
+Console.WriteLine(dockerResults.StandardOutput);
+
+var bat = await Cli.Wrap("demo.bat")
+    .ExecuteBufferedAsync();
+
+Console.WriteLine(bat.StandardOutput);
+
+var ps = await Cli.Wrap("powershell")
+    .WithArguments(@"./demo.ps1")
+    .ExecuteBufferedAsync();
+
+Console.WriteLine(ps.StandardOutput);
+
+var sh = await Cli.Wrap("bash")
+    .WithArguments(@"./demo.sh")
+    .ExecuteBufferedAsync();
+
+Console.WriteLine(sh.StandardOutput);
