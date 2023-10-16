7dc0e84277cb867a837264a7b2bf5a0e5596dbb5
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2023-10-16 11:24:27 +0300

    refactor, code generation error fix, version upgrade

--- AnnotationsAPI.Lambda/src/AnnotationsAPI.Lambda/Functions.cs
+++ AnnotationsAPI.Lambda/src/AnnotationsAPI.Lambda/Functions.cs
@@ -2,63 +2,52 @@
 
 namespace AnnotationsAPI.Lambda;
 
-/// <summary>
-/// A collection of sample Lambda functions that provide a REST api for doing simple math calculations.
-/// </summary>
 public class Functions
 {
     private readonly IConfiguration _configuration;
 
-    public Functions(IConfiguration configuration)
-    {
-        _configuration = configuration;
-    }
+    public Functions(IConfiguration configuration) => _configuration = configuration;
 
-    /// <summary>
-    /// Root route that provides information about the other requests that can be made.
-    /// </summary>
-    /// <returns>API descriptions.</returns>
     [LambdaFunction]
     [HttpApi(LambdaHttpMethod.Get, "/")]
-    public static string Default()
+    public string Default()
     {
-        var docs = @"Lambda Calculator Home:
-You can make the following requests to invoke other Lambda functions perform calculator operations:
-/add/{x}/{y}
-/subtract/{x}/{y}
-/multiply/{x}/{y}
-/divide/{x}/{y}
-";
+        var docs =
+            """
+                Lambda Calculator Home:
+                        You can make the following requests to invoke other Lambda functions perform calculator operations:
+                            /add/{x}/{y}
+                            /subtract/{x}/{y}
+                            /multiply/{x}/{y}
+                            /divide/{x}/{y}
+            """;
         return docs;
     }
 
     [LambdaFunction]
     [HttpApi(LambdaHttpMethod.Get, "/add/{x}/{y}")]
-    public static IHttpResult Add([FromServices] IConfiguration configuration, int x, int y, ILambdaContext context)
+    public IHttpResult Add([FromServices] IConfiguration configuration, int x, int y, ILambdaContext context)
     {
         context.Logger.LogInformation($"{x} plus {y} is {x + y}");
         return HttpResults.Ok((x + y).ToString());
     }
 
-    [LambdaFunction]
-    [HttpApi(LambdaHttpMethod.Get, "/subtract/{x}/{y}")]
-    public static int Subtract(int x, int y, ILambdaContext context)
+    [LambdaFunction, HttpApi(LambdaHttpMethod.Get, "/subtract/{x}/{y}")]
+    public int Subtract(int x, int y, ILambdaContext context)
     {
         context.Logger.LogInformation($"{x} subtract {y} is {x - y}");
         return x - y;
     }
 
-    [LambdaFunction]
-    [HttpApi(LambdaHttpMethod.Get, "/multiply/{x}/{y}")]
-    public static int Multiply(int x, int y, ILambdaContext context)
+    [LambdaFunction, HttpApi(LambdaHttpMethod.Get, "/multiply/{x}/{y}")]
+    public int Multiply(int x, int y, ILambdaContext context)
     {
         context.Logger.LogInformation($"{x} multiply {y} is {x * y}");
         return x * y;
     }
 
-    [LambdaFunction]
-    [HttpApi(LambdaHttpMethod.Get, "/divide/{x}/{y}")]
-    public static int Divide(int x, int y, ILambdaContext context)
+    [LambdaFunction, HttpApi(LambdaHttpMethod.Get, "/divide/{x}/{y}")]
+    public int Divide(int x, int y, ILambdaContext context)
     {
         context.Logger.LogInformation($"{x} divide {y} is {x / y}");
         return x / y;
--- AnnotationsAPI.Lambda/src/AnnotationsAPI.Lambda/GlobalUsings.cs
+++ AnnotationsAPI.Lambda/src/AnnotationsAPI.Lambda/GlobalUsings.cs
@@ -1,5 +1,3 @@
-// Global using directives
-
 global using Amazon.Lambda.Annotations;
 global using Amazon.Lambda.Annotations.APIGateway;
 global using Amazon.Lambda.Core;
