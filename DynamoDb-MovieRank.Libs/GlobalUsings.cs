5145265e09ded2bdc8288134f9676dc4113cd22b
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2023-10-16 11:24:38 +0300

    cleanup

--- DynamoDb-MovieRank.Libs/GlobalUsings.cs
+++ DynamoDb-MovieRank.Libs/GlobalUsings.cs
@@ -1,6 +1,4 @@
-﻿// Global using directives
-
-global using Amazon.DynamoDBv2;
+﻿global using Amazon.DynamoDBv2;
 global using Amazon.DynamoDBv2.DataModel;
 global using Amazon.DynamoDBv2.DocumentModel;
 global using Amazon.DynamoDBv2.Model;
